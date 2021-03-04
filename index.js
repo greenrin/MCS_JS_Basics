fc192d173348b841cea299790d4bacbf7359bbfc
Leonova N <porncrusader@gmail.com>
2021-03-04 22:50:50 +0300

    homework

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+let totalCash = prompt('how much money?');
+let watchesCount = prompt('how much watches?');
+let earringsCount = prompt('how much earrings?');
+let watchesSum = prompt('how much is watches cost?')*watchesCount;
+let earringsSum = prompt('how much is earrings cost?')*earringsCount;
+
+console.log(watchesSum+earringsSum<=totalCash);
+
+
+
